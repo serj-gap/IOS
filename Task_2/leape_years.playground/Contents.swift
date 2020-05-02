0fde93472e3a57e73ce1f57ef721005db6c3b4aa
sergey_titov <sierghiei.titov.00@mail.ru>
2020-05-02 13:09:55 +0300

    fixed mistakes

--- Task_2/leape_years.playground/Contents.swift
+++ Task_2/leape_years.playground/Contents.swift
@@ -1,28 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
-
 
 enum WeekDays: Int {
    
-    case Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7
+    case monday = 1, tuesday, wednesday, thursday, friday, saturday, sunday
 }
 
-func getDay (startDay: inout Int) -> WeekDays {
+
+func getDay (startDay: inout WeekDays.RawValue) -> WeekDays {
     
     let day = WeekDays(rawValue: startDay)
      startDay += 5
 
-    if startDay%7 == 0 {
-        startDay = 7
-    } else if startDay>7 {
-        startDay = startDay%7
-    }
+    startDay%7 == 0 ? startDay = 7 : (startDay = startDay%7)
     
     return day!
-    }
+    
+}
 
 var startDay = 3
+
 for years in 1801...2020 {
     if years % 4 == 0 || years % 400 == 0 && years % 100 != 0 {
         let day = getDay(startDay: &startDay)
