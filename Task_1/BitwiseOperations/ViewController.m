a8311fc3d19c368cdb332f15a9ad7041e08f763b
sergey_titov <sierghiei.titov.00@mail.ru>
2020-05-15 17:11:50 +0300

    first loading

---
+++ Task_1/BitwiseOperations/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BitwiseOperations
+//
+//  Created by Sergey Titov on 5/15/20.
+//  Copyright © 2020 Sergey Titov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
